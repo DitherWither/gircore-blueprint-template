32a3df4924a0fad0282f7f177fcf020c7866ae60
DitherWither <demonslayervardhan@gmail.com>
2023-03-17 13:17:07 +0530

    Initial Commit

---
+++ Justfile
@@ -0,0 +1,15 @@
+clean:
+    dotnet clean
+    rm -r ./build
+    rm -r ./bin
+    rm -r ./obj
+
+build-blueprints:
+    # This compiles the blueprint files into gtk ui files in a directory called ui
+    blueprint-compiler batch-compile build/ blueprints blueprints/**.blp
+
+build: build-blueprints
+    dotnet build
+
+run: build-blueprints
+    dotnet run
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿const string APP_ID = "io.github.ditherwither.blueprint-template";
+
+var app = Adw.Application.New(APP_ID, Gio.ApplicationFlags.DefaultFlags);
+app.OnActivate += (sender, args) => {
+    var mainWindow = new BlueprintTemplate.ui.MainWindow();
+
+    mainWindow.Application = (Adw.Application) sender;
+    mainWindow.Show();
+};
+
+return app.Run();
