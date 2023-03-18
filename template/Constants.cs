93bee762087cbbd50a862ce3237f811ae75d18d5
DitherWither <demonslayervardhan@gmail.com>
2023-03-18 14:16:29 +0530

    Move constants to a seperate file, add docs

---
+++ template/Constants.cs
@@ -0,0 +1,8 @@
+public static class Constants
+{
+    /// <summary>The application ID that is used to identify your application,
+    /// see https://developer.gnome.org/documentation/tutorials/application-id.html.
+    /// This should be automatically replaced when the application is created.
+    /// </summary>
+    public const string APP_ID = "__APP_ID__";
+}
--- template/ui/MainWindow.cs
+++ template/ui/MainWindow.cs
@@ -5,10 +5,13 @@ public class MainWindow : Adw.ApplicationWindow
     private MainWindow(Gtk.Builder builder, string name) : base(builder.GetPointer(name), false)
     {
         builder.Connect(this);
+
+        // Do any initialization, or connect signals here.
     }
 
-    public MainWindow() : this(new Gtk.Builder("MainWindow.ui"), "main_window")
-    {
 
+    public MainWindow(Adw.Application application) : this(new Gtk.Builder("MainWindow.ui"), "main_window")
+    {
+        this.Application = application;
     }
 }
